5383fac7f5709d68eee70c8fae9c40a6de7887b6
sakio318 <saki1803terashima@gail.com>
2020-11-24 14:17:37 +0000

    startmeshiterro

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '9ecfb71e5c3102cae11f2001dd062e2859d2042f60ff2cd6db9ff9fded2ee734f851fcecb09db260830b87c08c45d590a3127c99a6c4081e409688d542340b56'
