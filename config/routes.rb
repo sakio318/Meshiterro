2c7b198a6e0ca16a6dbb41744546003f3b8f88c7
sakio318 <saki1803terashima@gail.com>
2020-11-25 14:06:37 +0000

    finish4-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to:'homes#top'
 
@@ -6,4 +7,5 @@ resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   resource :favorites, only: [:create, :destroy]
   resources :post_comments, only: [:create, :destroy]
 end
+  resources :users, only: [:show, :edit,:update]
 end
